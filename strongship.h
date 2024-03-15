893bb0dc6fe32f8600ea93795e72c799d33dd85e
buseaydogan <157000212+buseaydogan@users.noreply.github.com>
2024-03-15 09:18:39 +0300

    Add files via upload

---
+++ strongship.h
@@ -0,0 +1,25 @@
+#ifndef UNTITLED_COMMONSHIP_H
+#define UNTITLED_COMMONSHIP_H
+#include "spaceships.h"
+
+class commonShip:public Spaceships {
+public:
+    //CONSTRUCTOR
+    commonShip(double _damageCoef):Spaceships(1){}
+
+    //UPDATES
+    int updateHealthDamage10(int& _health) override;
+    int updateHealthDamage30(int& _health) override;
+
+    //EVENTS
+    void asteroid() override;
+
+    //ACTIONS
+    void run() override;
+    void fight() override;
+
+private:
+protected:
+
+};
+#endif //UNTITLED_COMMONSHIP_H
